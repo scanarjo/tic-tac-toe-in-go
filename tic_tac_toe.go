e8ddc3aecd901f6bf9eb17c6296cd5f0a8b09b71
Scott Arden-Jones <4737932+scanarjo@users.noreply.github.com>
2025-08-02 12:34:37 +0100

    Randomise first player in the game

--- tic_tac_toe.go
+++ tic_tac_toe.go
@@ -1,15 +1,26 @@
 package tic_tac_toe
 
+import "math/rand"
+
 type Board [3][3]rune
 
 type Game struct {
-	Board Board
+	Board  Board
+	Next   rune
+	IsOver bool
 }
 
 func NewGame() *Game {
 	const BLANK = ' '
+	const X = 'X'
+	const O = 'O'
+
+	players := [2]rune{X, O}
+
+	firstPlayer := players[rand.Intn(2)]
 
 	return &Game{
+		Next: firstPlayer,
 		Board: Board{
 			{BLANK, BLANK, BLANK},
 			{BLANK, BLANK, BLANK},
--- tic_tac_toe_test.go
+++ tic_tac_toe_test.go
@@ -15,6 +15,29 @@ func TestNewGame(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(game.Board, freshBoard) {
-		t.Errorf("Expected %+v, but got %+v", freshBoard, game.Board)
+		t.Errorf("Expected %q, but got %q", freshBoard, game.Board)
 	}
+
+	if game.IsOver {
+		t.Error("Expected game to not be over, but it is")
+	}
+
+	if game.Next != 'X' && game.Next != 'O' {
+		t.Errorf("Game should start with 'X' or 'O' as next player, but got %q", game.Next)
+	}
+}
+
+func TestNewGameStartsWithRandomPlayer(t *testing.T) {
+	game := NewGame()
+
+	firstChosen := game.Next
+
+	for i := 0; i < 100; i++ {
+		game = NewGame()
+		if game.Next != firstChosen {
+			return
+		}
+	}
+
+	t.Errorf("Game should start with a random player, but always started with %q", firstChosen)
 }
